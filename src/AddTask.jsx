0b138a7396208f4f1fcc71337d86462087a3811d
AndreaBeltramin <andreabeltra99@gmail.com>
2025-03-24 23:12:35 +0100

    milestone 5

--- src/AddTask.jsx
+++ src/AddTask.jsx
@@ -1,8 +1,75 @@
+import { useState, useRef } from "react";
+
+const symbols = "!@#$%^&*()-_=+[]{}|;:'\\\",.<>?/`~";
+
 export default function AddTask() {
+	const [title, setTitle] = useState("");
+	const descriptionRef = useRef();
+	const statusRef = useRef();
+
+	const isTitleNotValid = [...title].some((char) => symbols.includes(char));
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		if (!title || isTitleNotValid) {
+			alert("Inserisci un titolo valido!");
+			return;
+		}
+
+		console.log("task", {
+			title,
+			description: descriptionRef.current.value,
+			status: statusRef.current.value,
+		});
+		setTitle("");
+		descriptionRef.current.value = "";
+		statusRef.current.value = "";
+	};
 	return (
 		<>
 			<div className="container mt-4">
-				<h1>titolo pagina aggiungi task</h1>
+				<h1>Form per aggiunta Task</h1>
+				<div className="row">
+					<div className="row-cols-2">
+						<form onSubmit={handleSubmit}>
+							<div className="mb-3">
+								<label htmlFor="titleTask" className="form-label">
+									Nome della task
+								</label>
+								<input
+									type="text"
+									className="form-control"
+									id="titleTask"
+									value={title}
+									onChange={(e) => setTitle(e.target.value)}
+								/>
+							</div>
+							<div className="mb-3">
+								<label htmlFor="descrizioneTask" className="form-label">
+									Descrizione
+								</label>
+								<textarea
+									className="form-control"
+									id="descrizioneTask"
+									ref={descriptionRef}
+								/>
+							</div>
+
+							<div className="mb-3">
+								<select className="form-select" id="selectTask" ref={statusRef}>
+									<option defaultValue="Seleziona uno status">
+										Seleziona uno status
+									</option>
+									<option value="To do">To do</option>
+									<option value="Doing">Doing</option>
+									<option value="Done">Done</option>
+								</select>
+							</div>
+							<button type="submit" className="btn btn-primary">
+								Submit
+							</button>
+						</form>
+					</div>
+				</div>
 			</div>
 		</>
 	);
